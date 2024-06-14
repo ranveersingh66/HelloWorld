929e6ef96e980974f79cc38f31b51eda8c92266b
Ranveersingh Pawar <ranveersingh.pawar6627@gmail.com>
2024-06-14 15:24:10 +0530

    Add files via upload

---
+++ HelloWorld/Math.cpp
@@ -0,0 +1,6 @@
+int Multiply(int a, int b)
+{
+	int result = a * b;
+		return result;
+#include "EndBrace.h"
+	// include directive? in C is interpreted by the preprocessor to include the header file's content. 
